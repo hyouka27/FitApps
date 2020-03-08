59e555734cb5ea45f4ebf4aed13aed6b79f1d872
nsawek <nsawek@gmail.com>
2020-03-08 20:02:20 +0100

    Zabezpieczenie przed złą zmianą danych

--- app/src/main/java/com/example/fitapps/UserEditActivity.kt
+++ app/src/main/java/com/example/fitapps/UserEditActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fitapps
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitapps.Database.DAOHelp
 import com.example.fitapps.Database.Users
@@ -12,16 +13,24 @@ class UserEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_edit)
-        val db = DAOHelp(this)
         btnsave.setOnClickListener {
             val editNam = editName.text.toString()
             val editGro=editGross.text.toString()
             val editAge=editAge.text.toString()
             val editHei=editHeight.text.toString()
             //Zapis danych do bazy
-            db.getUserDao().updateUsers(Users(1,editNam,editAge.toInt(),editHei.toInt(),editGro.toInt()))
-            val intent = Intent(this, UserActivity::class.java)
-            startActivity(intent)
+            if ((editNam=="")||(editGro=="")||(editHei=="")||(editAge=="")){
+                val intent = Intent(this, UserActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this, "Błędne dane", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val db = DAOHelp(this)
+                db.getUserDao().updateUsers(Users(1,editNam,editAge.toInt(),editHei.toInt(),editGro.toInt()))
+                val intent = Intent(this, UserActivity::class.java)
+                startActivity(intent)
+            }
+
         }
 
     }
